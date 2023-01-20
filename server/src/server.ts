ceac6cc8558965c651d97880699045530c3c06ee
jfernandesdev <jfernandes.dev@gmail.com>
2023-01-20 11:30:44 -0300

    CHORE: back-end setup with fastify, typescript, prism and cors

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habits.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('🚀 Server running in port 3333...')
+})
+
